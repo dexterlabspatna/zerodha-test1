000602d154b4e58a5aa7a25d50d95ea85836d3fd
dexterlabspatna <88551522+dexterlabspatna@users.noreply.github.com>
2021-10-22 11:52:28 +0530

    Add files via upload

---
+++ kitesettings.py
@@ -0,0 +1,5 @@
+
+API_KEY = "bqmjxav65do7xw8w"
+api_secret = "q5th4lfpsd1jbp944b8t0zpvn5tfffin"
+# ====================================================
+access_token = "dPBTxI90ata52CoIhv6OY6R1C9TOq4hb"

e01fefd1742537557fcdc1a28df8e93f982092ce
dexterlabspatna <88551522+dexterlabspatna@users.noreply.github.com>
2021-10-27 21:29:55 +0530

    Update kiteGetAccessToken.py

--- kiteGetAccessToken.py
+++ kiteGetAccessToken.py
@@ -1,16 +1,12 @@
-import logging
 import kitesettings
 from kiteconnect import KiteConnect
-
-logging.basicConfig(level=logging.DEBUG)
+import webbrowser
 
 kite = KiteConnect(kitesettings.API_KEY)
-
-# https://kite.zerodha.com/connect/login?v=4&API_KEY=Q8JPzjkt8ftXgqvmXa
-
-request_token = input("Request Token: ")
-data = kite.generate_session(request_token, kitesettings.api_secret)
-kite.set_access_token(data["access_token"])
-
-print("====================")
-print("Access Token: ", data["access_token"])
+print(kite.login_url())
+webbrowser.open(kite.login_url(), new=0)
+reqt_token = input("Request Token: ")
+gen_ssn = kite.generate_session(request_token=reqt_token, api_secret=kitesettings.api_secret)
+acc_tok = gen_ssn['access_token']
+print("Access Token: ", acc_tok)
+kite.set_access_token(acc_tok)

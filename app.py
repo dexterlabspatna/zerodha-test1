ced9d245494d1cd2fe10cd4dd18657f3bf1a5997
dexterlabspatna <88551522+dexterlabspatna@users.noreply.github.com>
2021-10-29 01:24:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,3 +50,28 @@ def webhook():
         "code": "error",
         "message": "order"
     }
+@app.route('/options', methods=['POST'])
+def webhook1():
+    print(request.data)
+    data = json.loads(request.data)
+    kite.set_access_token(kitesettings.access_token)
+
+    try:
+        order_id = kite.place_order(tradingsymbol="NIFTY21NOV18200CE",
+                                    exchange="NFO",
+                                    transaction_type="BUY",
+                                    quantity="50",
+                                    price="0.0",
+                                    variety=kite.VARIETY_REGULAR,
+                                    order_type="SL-M",
+                                    product=kite.PRODUCT_NRML,
+				    trigger_price=7)
+        print("Order placed. ID is: {}".format(order_id))
+    except Exception as e:
+        print("Order placement failed: {}".format(e))
+
+    return order_id
+    return{
+        "code": "error",
+        "message": "order"
+    }
